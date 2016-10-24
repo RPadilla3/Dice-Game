86a303a476609257791b5befdfb284ed86a4585d
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-24 16:16:45 -0400

    Dice works in the console but Still not connected to the html elements

--- dicegame.js
+++ dicegame.js
@@ -5,22 +5,30 @@
     // Any HTML element has an `innerText` property that you can
     // access (or change) for the contents of that element.
 
+
     document.querySelector('button').addEventListener('click', function buttonClicked() {
+
+
+
+    var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
 
       var dieOne = Math.floor(Math.random()* 6 + 1);
       console.log(dieOne);
       var dieTwo = Math.floor(Math.random()* 6 + 1);
       console.log(dieTwo);
-      var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
+      var score = dieOne + dieTwo;
+      console.log(score);
 
 
-
-
-
-
-
-
+      if (score === 11) {
+        alert("You Win");
+      }
+      else if (score === 7) {
+        alert("You Win!");
+      }
     });
+
+
 
 
 })();
